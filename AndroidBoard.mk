22286efdae95b586de5be1bc47c2c27614ad6393
Jed Davis <jld@mozilla.com>
2014-05-28 14:03:07 -0700

    Bug 1015158 - Fork device/qcom/msm8610/fstab.qcom.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,1 +1,11 @@
 include device/t2m/flame/msm8610-AndroidBoard.mk
+
+LOCAL_PATH := device/t2m/flame
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
